5b4f09875611af6cfbb1368b96eaa96e92a037a0
McTysonClebert <mctysonclebert@gmail.com>
2022-12-22 20:22:57 -0500

    View Vard

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -12,8 +12,18 @@ const Nav = () => {
 
   return (
     <nav className="bg-black text-white h-auto p-4 md:p-8 flex flex-col md:flex-row md:justify-between items-center gap-4">
-      <Link to={"/"}>
-        <h1 className="font-bold text-2xl">Auth Card System</h1>
+      <Link
+        to={"/"}
+        className="flex flex-col justify-center items-center md:flex-row gap-2"
+      >
+        <div className="bg-white w-12 md:w-16  h-12 md:h-16  rounded-full overflow-hidden">
+          <img
+            src="/assets/logo.jpeg"
+            alt=""
+            className="w-12 md:w-16  h-12 md:h-16 "
+          />
+        </div>
+        <h1 className="font-bold text-2xl">MAVIE</h1>
       </Link>
       <ul className="flex items-center justify-center w-full md:w-1/2 md:justify-end">
         {user && (
