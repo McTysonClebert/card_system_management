3d75b1d1b100dadcda7ee772e272dc9f9a20caf7
McTysonClebert <mctysonclebert@gmail.com>
2022-12-22 19:16:27 -0500

    View Vard

--- client/src/pages/CardExport.jsx
+++ client/src/pages/CardExport.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import QrCode from "qrcode";
-import html2canvas from "html2canvas";
+
 import {
   WhatsappIcon,
   WhatsappShareButton,
@@ -16,30 +16,53 @@ import {
 
 const CardExport = () => {
   const { id } = useParams();
-  const [qrCode, setQrCode] = useState();
   const imageRef = useRef();
-  const [image, setImage] = useState();
+  const [downloadLink, setDownloadLink] = useState(null);
 
   useEffect(() => {
-    const url = `${window.location.origin}/view/${id}`;
-    QrCode.toDataURL(url)
-      .then((data) => {
-        setQrCode(data);
-      })
-      .catch((err) => {
-        console.log(`Error generating qr code: ${err.message}`);
-      });
+    const canvas = imageRef.current;
+    const context = canvas.getContext("2d");
+    const bg = document.createElement("img");
+
+    bg.src = "/assets/card.png";
+
+    bg.onload = () => {
+      canvas.width = bg.width;
+      canvas.height = bg.height;
+
+      context.globalAlpha = 1.0;
+
+      const url = `${window.location.origin}/view/${id}`;
+
+      QrCode.toDataURL(url, { width: "16px", height: "16px" })
+        .then((data) => {
+          const qc = document.createElement("img");
+          qc.src = data;
+
+          qc.onload = () => {
+            context.drawImage(bg, 0, 0);
+            context.drawImage(qc, bg.width - qc.width, bg.height - qc.height);
+
+            const dataUrl = canvas.toDataURL("image/png", true);
+
+            const cardImage = document.createElement("img");
+            cardImage.src = dataUrl;
+
+            const link = document.createElement("a");
+            link.href = cardImage.src;
+            link.download = "card.png";
+            setDownloadLink(link);
+          };
+        })
+        .catch((err) => {
+          console.log(`Error generating qr code: ${err.message}`);
+        });
+    };
   }, []);
 
-  useEffect(() => {
-    html2canvas(imageRef.current, {
-      scale: 2
-    })
-      .then((canvas) => canvas.toDataURL("image/jpeg", 1.0))
-      .catch((err) => {
-        console.log(`Error creating image ${err.message}`);
-      });
-  }, []);
+  const handleDownload = () => {
+    downloadLink.click();
+  };
 
   return (
     <div className="flex flex-col bg-slate-800 w-screen h-screen p-4">
@@ -47,55 +70,14 @@ const CardExport = () => {
         Card Export
       </p>
 
-      {/* <div
-        className="flex w-full h-2/5 md:w-2/4 md:h-3/6 mx-auto text-white overflow-hidden shadow-sm shadow-white relative z-20 p-2 md:p-4 bg-[url('/assets/bg-1.jpeg')] bg-cover rounded-lg"
-        ref={imageRef}
-      >
-        <div className="h-full flex flex-col items-center justify-around">
-          <p className="font-bold text-2xl md:text-4xl">Gala De Noel</p>
-          <img
-            src="/assets/logo.jpeg"
-            alt="logo"
-            className="w-16 h-16 rounded-full md:w-20 md:h-20"
-          />
-          <p className="text-gray-50 text-xl md:text-2xl drop-shadow-lg">
-            Life Changing
-          </p>
-        </div>
-        <div className="w-16 h-16 md:w-20 md:h-20 bg-black absolute bottom-3 right-3 md:bottom-5 md:right-5">
-          <img src={qrCode} alt="QrCode" />
-        </div>
-      </div> */}
+      <canvas ref={imageRef}></canvas>
 
-      <div
-        className="flex justify-center items-center mx-auto relative"
-        ref={imageRef}
+      <button
+        onClick={handleDownload}
+        className="bg-sky-700 text-white font-bold my-8 mx-auto rounded-lg py-2 px-4"
       >
-        <img
-          className="absolute bottom-0 right-0 w-16 h-16 md:w-24 md:h-24 z-50"
-          src={qrCode}
-          alt="QrCode"
-        />
-        <img src="/assets/card.png" alt="card" />
-      </div>
-
-      <div className="my-10 text-white md:w-2/4 mx-auto">
-        <p className="my-2 text-lg">Share With:</p>
-        <div className="flex gap-4 justify-between md:justify-start">
-          <WhatsappShareButton url={image}>
-            <WhatsappIcon round></WhatsappIcon>
-          </WhatsappShareButton>
-          <FacebookShareButton url={image}>
-            <FacebookIcon round></FacebookIcon>
-          </FacebookShareButton>
-          <TelegramShareButton url={image}>
-            <TelegramIcon round></TelegramIcon>
-          </TelegramShareButton>
-          <FacebookMessengerShareButton url={image}>
-            <FacebookMessengerIcon round></FacebookMessengerIcon>
-          </FacebookMessengerShareButton>
-        </div>
-      </div>
+        Download Card
+      </button>
     </div>
   );
 };
